dfbfb1fb3d9dd27149465ccade650028434ad67d
baiwenhao <baiwenhao2022@qq.com>
2022-11-26 14:32:50 +0800

    third commit

--- src/main/java/com/haoge/bean/Email.java
+++ src/main/java/com/haoge/bean/Email.java
@@ -1,5 +1,7 @@
 package com.haoge.bean;
 
+import java.util.HashSet;
+
 /**
  * @author haoge
  * @version 1.0.0
@@ -10,7 +12,11 @@ public class Email {
     private String hello;
 
     public static void main(String[] args) {
+
+        HashSet<Object> objects = new HashSet<>();
         System.out.println("hello git");
+        System.out.println("hello git1");
+        System.out.println("hello git3");
     }
 
 }
